7b96b9e85075daa3c0ffd925a9f77dc516907b5a
Starry_Fang <py_p@163.com>
2024-01-27 13:45:23 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+﻿from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+__all__ = ['errors', 'stream_readers', 'stream_writers', 'task']
---
+++ _task_modules/channels/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+__author__ = 'Art Technology'
+__all__ = ['channel']
+
+#from artdaq._task_modules.channels.do_channel import DOChannel
